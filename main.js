78c49833ad8ae7ae6a6fdc4e9773a6b682bfe789
vitoria <107407941+ladybug2464@users.noreply.github.com>
2024-04-29 12:32:13 -0300

    HJH

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataAtual = new Date ();
-let dataObjetivo=prompt("sata final ANO-MES-DIA exemplo:2021=-04=-10");
+let dataObjetivo=prompt("Data final ANO-MES-DIA exemplo:2021=-04=-10");
 dataObjetivo=new date (dataObjetivo + "T23:59:59");
-let diasQueFaltam=math.floor ((dataObjetivo- dataAtual)/86400000);
+let diasQueFaltam = Math.floor ((dataObjetivo- dataAtual)/86400000);
 document.querySelector("#dias_restantes").textcontent=diasQueFaltam;
